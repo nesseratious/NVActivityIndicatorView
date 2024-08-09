54866dcd057c73d3eb0abb22a6ba02db41d66cc2
Denis Esie <nesseratious@gmail.com>
2024-08-09 23:46:40 +0300

    Remove tests, update spm versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NVActivityIndicatorView",
     platforms: [
-        .iOS(.v9),
-        .tvOS(.v9)
+        .iOS(.v16), .macCatalyst(.v16), .tvOS(.v16)
     ],
     products: [
         .library(name: "NVActivityIndicatorView", targets: ["NVActivityIndicatorView"]),
